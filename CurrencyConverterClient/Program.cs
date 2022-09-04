bd068b001a92fb0422cfbd8674d1e455b3e03400
Seymur Bagirov <megadestroyer230@gmail.com>
2022-09-05 02:56:08 +0400

    add changes

--- CurrencyConverterClient/Program.cs
+++ CurrencyConverterClient/Program.cs
@@ -8,32 +8,83 @@ using CurrencyConverterClient;
 
 using var client = new TcpClient();
 
-await client.ConnectAsync(IPEndPoint.Parse("127.0.0.1:13377"));
+try
+{
+    await client.ConnectAsync(IPEndPoint.Parse("127.0.0.1:13377"));
+}
+catch (SocketException)
+{
+    Console.WriteLine("Couldn't connect to the server. Closing the program");
+    await Task.Delay(1500);
+    return;
+}
 
 await using var writer = new StreamWriter(client.GetStream())
 {
     AutoFlush = true
 };
-var showMenu = false;
+var showMenu = true;
 using var reader = new StreamReader(client.GetStream());
+var set = new HashSet<string>() { "azn", "eur", "usd", "uah" };
 while (true)
 {
-    if (showMenu is true)
+    if (showMenu)
     {
-        Console.Write("AZN(azn)\nEUR(eur)\n3.USD(usd)\n4.UAH(uah)\n> ");
+        Console.WriteLine("Example: azn to uah 2");
+        Console.WriteLine("AZN(azn)\nEUR(eur)\nUSD(usd)\nUAH(uah)\nClose the program(close)");
     }
+    Console.Write("> ");
     var str = Console.ReadLine();
-    var request = new ConversionRequest("EUR", "AZN", 3m);
+    if (str == "close")
+    {
+        return;
+    }
+    var arguments = str?.Split(' ');
+    if (arguments is null)
+    {
+        showMenu = false;
+        continue;
+    }
+    if (!ValidateArguments(arguments, set))
+    {
+        showMenu = false;
+        continue;
+    }
+    showMenu = true;
+    var request = new ConversionRequest(arguments[0].ToUpper(), arguments[2].ToUpper(), 
+        decimal.Parse(arguments[3]));
     await HandleRequest(writer, reader, request);
 }
 
-async Task HandleRequest(StreamWriter streamWriter, StreamReader streamReader, ConversionRequest request)
+async Task HandleRequest(TextWriter streamWriter, TextReader streamReader, ConversionRequest request)
 {
     await streamWriter.WriteLineAsync(JsonSerializer.Serialize(request));
     var json = await streamReader.ReadLineAsync();
     var memStream = new MemoryStream(Encoding.UTF8.GetBytes(json!));
     var conversionResult = await JsonSerializer.DeserializeAsync<ConversionResult>(memStream);
+    Console.Clear();
     Console.WriteLine($"Result of conversion from {request.MoneyToConvert} {request.From} " +
                       $"to {request.To} = {conversionResult?.Result}");
 }
 
+bool ValidateArguments(IReadOnlyList<string> args, IReadOnlySet<string> filters)
+{
+    if (args.Count is not 4)
+    {
+        return false;
+    }
+
+    if (args[0] == args[2])
+        return false;
+
+    if (!filters.Contains(args[0]))
+        return false;
+
+    if (args[1] != "to")
+        return false;
+
+    if (!filters.Contains(args[2]))
+        return false;
+
+    return args[3].All(char.IsDigit);
+}
